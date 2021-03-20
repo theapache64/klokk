35520277c4a14b9a82f34fa03bf3c4fc7c7ae20a
theapache64 <theapache64@gmail.com>
2021-03-20 14:38:35 +0530

    🌟 add bulk animation

--- src/main/kotlin/com/theapache64/kineticwallclock/composable/Clock.kt
+++ src/main/kotlin/com/theapache64/kineticwallclock/composable/Clock.kt
@@ -1,24 +1,24 @@
 package com.theapache64.kineticwallclock.composable
 
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.animateIntAsState
+import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.tween
 import androidx.compose.desktop.Window
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import com.theapache64.kineticwallclock.theme.CodGray
 import com.theapache64.kineticwallclock.theme.CodGray_2
 import kotlin.math.cos
 import kotlin.math.sin
-
 
 fun main(args: Array<String>) {
     Window {
@@ -28,28 +28,17 @@ fun main(args: Array<String>) {
         Box(
             modifier = Modifier.fillMaxSize().background(CodGray)
         ) {
-            val animationSpec = tween<Int>(500, easing = LinearEasing)
-
-            val targetOne by animateIntAsState(
-                needleOneDegree,
-                animationSpec = animationSpec,
-            )
-
-            val targetTwo by animateIntAsState(
-                needleTwoDegree,
-                animationSpec = animationSpec,
-            )
 
             Clock(
-                _needleOneDegree = targetOne,
-                _needleTwoDegree = targetTwo,
-                modifier = Modifier.fillMaxSize()
+                _needleOneDegree = needleOneDegree,
+                _needleTwoDegree = needleTwoDegree,
+                modifier = Modifier.size(600.dp)
             )
 
             Button(
                 onClick = {
-                    needleOneDegree = (0..360).random()
-                    needleTwoDegree = (0..360).random()
+                    needleOneDegree += 90
+                    needleTwoDegree += 180
                 }
             ) {
                 Text(text = "Animate")
@@ -58,58 +47,74 @@ fun main(args: Array<String>) {
     }
 }
 
-private const val NEEDLE_WIDTH = 30f
-private const val NEEDLE_HALF = NEEDLE_WIDTH * 0.5f
+
+private val NEEDLE_COLOR = Color.White
+private val NEEDLE_ANIMATION_SPEC = tween<Float>(500, easing = LinearEasing)
+
 
 @Composable
 fun Clock(
     _needleOneDegree: Int = 270,
     _needleTwoDegree: Int = 0,
-    modifier: Modifier = Modifier,
+    modifier: Modifier = Modifier
 ) {
 
-    val needleOneDegree = _needleOneDegree * Math.PI / 180
-    val needleTwoDegree = _needleTwoDegree * Math.PI / 180
+    val needleOneDegree = (_needleOneDegree * Math.PI / 180).toFloat()
+    val needleTwoDegree = (_needleTwoDegree * Math.PI / 180).toFloat()
+
+    val targetOne by animateFloatAsState(
+        needleOneDegree,
+        animationSpec = NEEDLE_ANIMATION_SPEC,
+    )
+
+    val targetTwo by animateFloatAsState(
+        needleTwoDegree,
+        animationSpec = NEEDLE_ANIMATION_SPEC,
+    )
 
     Canvas(
         modifier = modifier
     ) {
 
+        val needleWidth = size.minDimension * 0.05f
+
         // Background
         val radius = size.minDimension / 2f
+
         drawCircle(
             color = CodGray_2,
             radius = radius
         )
 
-        // Needle One
-
-        drawLine(
-            color = Color.White,
-            start = Offset(
-                x = center.x,
-                y = center.y,
-            ),
-            end = Offset(
-                x = center.x + radius * sin(needleOneDegree).toFloat(),
-                y = center.y + radius * cos(needleOneDegree).toFloat(),
-            ),
-            strokeWidth = NEEDLE_WIDTH
+        drawCircle(
+            color = NEEDLE_COLOR,
+            radius = needleWidth * 0.487f
         )
+
+
+        // Needle One
+        drawLine(
+            color = NEEDLE_COLOR,
+            start = center,
+            end = Offset(
+                x = center.x + radius * sin(targetOne),
+                y = center.y + radius * cos(targetOne),
+            ),
+            strokeWidth = needleWidth
+        )
+
 
         // Needle two
         drawLine(
-            color = Color.White,
-            start = Offset(
-                x = center.x,
-                y = center.y,
-            ),
+            color = NEEDLE_COLOR,
+            start = center,
             end = Offset(
-                x = center.x + radius * sin(needleTwoDegree).toFloat(),
-                y = center.y + radius * cos(needleTwoDegree).toFloat(),
+                x = center.x + radius * sin(targetTwo),
+                y = center.y + radius * cos(targetTwo),
             ),
-            strokeWidth = NEEDLE_WIDTH
+            strokeWidth = needleWidth
         )
+
     }
 
 }
--- src/main/kotlin/com/theapache64/kineticwallclock/main.kt
+++ src/main/kotlin/com/theapache64/kineticwallclock/main.kt
@@ -1,20 +1,65 @@
 import androidx.compose.desktop.Window
-import androidx.compose.material.Text
+import androidx.compose.desktop.WindowEvents
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
-import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntSize
+import androidx.compose.ui.unit.dp
+import com.theapache64.kineticwallclock.composable.Clock
 
-fun main() = Window {
-    var text by remember { mutableStateOf("Hello, World!") }
+private const val COLUMNS = 15
+private const val ROWS = 8
 
-    MaterialTheme {
-        Button(onClick = {
-            text = "Hello, Desktop!"
-        }) {
-            Text(text)
+private const val DIGIT_COLUMNS = 3
+private const val DIGIT_ROWS = 6
+
+fun main() {
+    var windowSize by mutableStateOf(IntSize.Zero)
+    var degOne by mutableStateOf(0)
+    var degTwo by mutableStateOf(0)
+
+    Window(
+        events = WindowEvents(
+            onResize = {
+                windowSize = it
+            }
+        ),
+    ) {
+
+        val clockWidth = windowSize.width / COLUMNS
+        val clockHeight = windowSize.height / ROWS
+        Column(
+            modifier = Modifier.fillMaxSize().background(Color.Black)
+        ) {
+            for (i in 0 until ROWS) {
+                Row {
+                    for (j in 0 until COLUMNS) {
+                        Clock(
+                            _needleOneDegree = degOne,
+                            _needleTwoDegree = degTwo,
+                            modifier = Modifier.size(
+                                width = clockWidth.dp,
+                                height = clockHeight.dp
+                            ).padding(10.dp)
+                        )
+                    }
+                }
+            }
+        }
+
+        Button(
+            onClick = {
+                degOne = (0..360).random()
+                degTwo = (0..360).random()
+            }
+        ){
+            Text(text = "Animate")
         }
     }
 }
